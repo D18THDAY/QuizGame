c50ac5a580caec351c05806bfde702ac040898b8
d18thday <sofoluweseun@gmail.com>
2020-05-04 02:05:40 +0100

    script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-//Functions
+
 function buildQuiz(){
-  // variable to store the HTML output
+  
   const output = [];
 
-  // for each question...
+  
   myQuestions.forEach(
     (currentQuestion, questionNumber) => {
 
-      // variable to store the list of possible answers
+      
       const answers = [];
 
-      // and for each available answer...
+    
       for(letter in currentQuestion.answers){
 
-        // ...add an HTML radio button
+        
         answers.push(
           `<label class="options">
             <input type="radio" name="question${questionNumber}" value="${letter}" class="option">
@@ -23,7 +23,7 @@ function buildQuiz(){
         );
       }
 
-      // add this question and its answers to the output
+      
       output.push(
 		  `<div class="slide">
 		    <div class="question"> ${currentQuestion.question} </div>
@@ -33,42 +33,38 @@ function buildQuiz(){
     }
   );
 
-  // finally combine our output list into one string of HTML and put it on the page
+  
   quizContainer.innerHTML = output.join('');
 }
 
 function showResults(){
 
-  // gather answer containers from our quiz
+  
   const answerContainers = quizContainer.querySelectorAll('.answers');
 
-  // keep track of user's answers
+  
   let numCorrect = 0;
 
-  // for each question...
+  
   myQuestions.forEach( (currentQuestion, questionNumber) => {
 
-    // find selected answer
+    
     const answerContainer = answerContainers[questionNumber];
     const selector = `input[name=question${questionNumber}]:checked`;
     const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-    // if answer is correct
     if(userAnswer === currentQuestion.correctAnswer){
-      // add to the number of correct answers
+      
       numCorrect++;
 
-      // color the answers green
-      answerContainers[questionNumber].style.color = 'lightgreen';
+      answerContainers[questionNumber].style.color = 'green';
     }
-    // if answer is wrong or blank
     else{
-      // color the answers red
+  
       answerContainers[questionNumber].style.color = 'red';
     }
   });
 
-  // show number of correct answers out of total
   resultsContainer.innerHTML = `<h2>You scored ${numCorrect} out of ${myQuestions.length}</h2>`;
 }
 
@@ -94,7 +90,6 @@ function showSlide(n) {
 }
 
 
-//Variables
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
@@ -151,7 +146,6 @@ const myQuestions = [
 buildQuiz();
 
 
-//Pagination
 const previousButton = document.getElementById("previous");
 const nextButton = document.getElementById("next");
 const slides = document.querySelectorAll(".slide");
@@ -159,7 +153,6 @@ let currentSlide = 0;
 let options = document.getElementsByClassName('option');
 
 
-//Show first slide
 showSlide(currentSlide);
 
 function showNextSlide() {
@@ -170,8 +163,6 @@ function showPreviousSlide() {
   showSlide(currentSlide - 1);
 }
 
-
-// Event listeners
 submitButton.addEventListener('click', showResults);
 
 previousButton.addEventListener("click", showPreviousSlide);
